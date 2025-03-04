6ee05caaa466e5d975b6f521c37f78b7d192e87e
Natia <karamnishvili.natia@gmail.com>
2025-03-04 15:12:40 +0400

    added script from js, done styling for mobile version

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const productsElement = document.querySelector('.products')
 const products = [
     {
        "image": {
@@ -99,4 +100,40 @@ const products = [
         "price": 6.50
      }
 ]
-console.log(products)
+const order =  [];
+
+let result = ""
+
+products.forEach(product => {
+const {image, category, name, price} = product;
+// const {} = image
+
+result += product.innerHTML = `    <div class="product">
+      <img src="${image.mobile}" class="product-img" alt="">
+      <button class="add-btn" value='${name}'>
+        <svg xmlns="http://www.w3.org/2000/svg" width="21" height="20" fill="none" viewBox="0 0 21 20"><g fill="#C73B0F" clip-path="url(#a)"><path d="M6.583 18.75a1.25 1.25 0 1 0 0-2.5 1.25 1.25 0 0 0 0 2.5ZM15.334 18.75a1.25 1.25 0 1 0 0-2.5 1.25 1.25 0 0 0 0 2.5ZM3.446 1.752a.625.625 0 0 0-.613-.502h-2.5V2.5h1.988l2.4 11.998a.625.625 0 0 0 .612.502h11.25v-1.25H5.847l-.5-2.5h11.238a.625.625 0 0 0 .61-.49l1.417-6.385h-1.28L16.083 10H5.096l-1.65-8.248Z"/><path d="M11.584 3.75v-2.5h-1.25v2.5h-2.5V5h2.5v2.5h1.25V5h2.5V3.75h-2.5Z"/></g><defs><clipPath id="a"><path fill="#fff" d="M.333 0h20v20h-20z"/></clipPath></defs></svg>
+Add to cart
+      </button>
+      <h3 class="category">${category}</h3>
+      <h2 class="product-name">${category}</h2>
+      <h3 class="price">$${price}</h3>
+    </div>`
+})
+productsElement.innerHTML = result
+
+const addBtns = document.querySelectorAll('.add-btn')
+const addItemHandler = (productName) => {
+const clickedProduct = products.find((item) => productName === item.name )
+console.log(clickedProduct)
+
+}
+
+addBtns.forEach(button => {
+    button.addEventListener('click', () => {
+        addItemHandler(button.value)
+    })
+})
+
+
+//obieqtebi gadaketdeba adviald roca rames vcvlit da vutolebt sxva obieqts amitom vikennebt spread operators, 
+//xolo masivebi ase advilad ar itvleba radgan sxvadasxva misamartzea shenaxuli
